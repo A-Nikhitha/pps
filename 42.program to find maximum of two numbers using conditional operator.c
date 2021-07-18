1a4a400b43034b9d28cfbc846eb69bf5071924d0
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-18 23:06:09 +0530

    Add files via upload

---
+++ 42.program to find maximum of two numbers using conditional operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+       int a,b;
+       printf("enter num");
+       scanf("%d",&a);
+       printf("enter other num");
+       scanf("%d",&b);
+       (a>b)?printf("max=%d",a):printf("max=%d",b);
+}
