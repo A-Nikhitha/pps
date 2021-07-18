ca4a359a1283d4fd229670e054dba8fd50ff47ec
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-18 13:12:41 +0530

    Add files via upload

---
+++ write a c program that accepts 3 integers from the user and          calculate the sum, product average of these 3 intrgers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c, sum;
+    float d;
+    printf("please enter 3 numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    sum=a+b+c;
+    d=(a+b+c/3);
+    printf("\nsum is %d;",sum);
+    printf("\nAverage is %f", d);
+    return 0;
+}
+    
+    
