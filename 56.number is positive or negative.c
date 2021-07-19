716d6a6f43dd8d578c1529268a80560fa03f1ed5
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-19 16:10:02 +0530

    Add files via upload

---
+++ 56.number is positive or negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+        int num;
+        printf("enter the inpu");
+        scanf("%d",&num);
+        if(num>0)
+        printf("the number=%d is positive",num);
+        else
+        printf("the number=%d is negative",num);
+}
