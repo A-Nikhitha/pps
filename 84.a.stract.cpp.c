e62788adb7ca245f42a3b93bde65857c1c8fc64d
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-20 01:31:04 +0530

    Add files via upload

---
+++ 84.a.stract.cpp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+        char w1[50],w2[20],ans;
+        printf("\nenter the w1= ");
+        gets(w1);
+        printf("\nenter the w2=");
+        gets(w2);
+        strcat(w1, w2);
+        printf("w1 after concatenation=%s",w1);
+}
+        
+        
+  
+      
+        
