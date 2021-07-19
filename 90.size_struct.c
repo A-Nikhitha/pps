488392c580a0b786c144b15edda7c302e8b093d3
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-20 01:52:05 +0530

    Add files via upload

---
+++ 90.size_struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct student
+{
+        int num;
+        char c[15];
+        float f;
+ }s;
+ int main()
+ {
+         int size;
+         size=sizeof(s);
+         printf("%d",size);
+}
+        
+        
+  
+      
+        
