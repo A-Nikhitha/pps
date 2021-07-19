4e3c9d3cd7e06598fe0df49dc066815f7545984c
A-Nikhitha <85937056+A-Nikhitha@users.noreply.github.com>
2021-07-19 22:05:41 +0530

    Add files via upload

---
+++ 72.a_z.cpp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+        char a;
+        printf("enter the value");
+        scanf("%c",&a);
+        if('a'<=a&&a<='z'||'A'<=a&&a<='Z');
+        printf("the given char=%c is an alphabet",a);
+}
+      
+        
